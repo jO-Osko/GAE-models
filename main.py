a41988e7e50a301d911ff9a7d40975ff6dd9c113
Filip Koprivec <koprivec.filip@gmail.com>
2018-03-03 15:57:52 +0100

    Urejanje soporocil

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ class RezultatHandler(BaseHandler):
 
 class ListHandler(BaseHandler):
     def get(self):
-        # Iz baze vzamemo vsa sporocila
-        seznam = Sporocilo.query().fetch()
+        # Iz baze vzamemo vsa sporocila, ki imajo polje izbrisano nastavljena na False
+        seznam = Sporocilo.query(Sporocilo.izbrisano == False).fetch()
         params = {"seznam": seznam}
         return self.render_template("seznam.html", params=params)
 
@@ -59,12 +59,56 @@ class PosameznoSporociloHandler(BaseHandler):
         return self.render_template("posamezno_sporocilo.html", params=params)
 
 
+class UrediHandler(BaseHandler):
+    def get(self, sporocilo_id):
+        # Ideja: vzamemo sporocilo iz baze
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+        params = {"sporocilo": sporocilo}
+        # In ga pokazemo
+        return self.render_template("uredi_sporocilo.html", params=params)
+
+    def post(self, sporocilo_id):
+        # Vzamemo sporocilo iz baze
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+        # sporocilo posodobimo
+        sporocilo.besedilo = self.request.get("nov-text")
+        # in ga shranimo nazaj v bazo
+        sporocilo.put()
+        # Uporabnika preusmerimo nazaj na seznam sporocil (po imenu)
+        # Pomen preusmerjanja po imenu je v tem, da se lahko pot povezave spremeni
+        # ime pa ostane enako in ne potrebuje ospreminjati poti na vec mestih
+        self.redirect_to("seznam-sporocil")
+
+
+# Dodatno:
+# na strani https://stackoverflow.com/questions/6515502/javascript-form-submit-confirm-or-cancel-submission-dialog-box
+# prvi ogovor pokaze kako lahko s pomocjo javascripta pokazemo potrditveno sporocilo
+class IzbrisiHandler(BaseHandler):
+    def get(self, sporocilo_id):
+        # Vzamemo sporocilo ven
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+        params = {"sporocilo": sporocilo}
+        # Prikazemo potrditveno spletno stran
+        # Ker si posljemo sporocilo, lahko pokazemo se kaksno informacijo o sporocilu
+        return self.render_template("izbrisi.html", params=params)
+
+    def post(self, sporocilo_id):
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+        # Sporocila ne izbrisemo, ampak ga zgolj "skrijemo"
+        sporocilo.izbrisano = True
+        # In zapisemo nazaj v bazo
+        sporocilo.put()
+        self.redirect_to("seznam-sporocil")
+
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/rezultat', RezultatHandler),
-    webapp2.Route('/seznam', ListHandler),
+    # pot poimenujemo, da se lahko sklicemo nanjo ko preusmerimo uporabnika
+    webapp2.Route('/seznam', ListHandler, name="seznam-sporocil"),
     # Na to pot primejo linki oblike /sporocilo/{{poljubne stevke}}
     # sporocilo_id pa je zgolj poimenovanje, ki ga potem uporabimo v metodi,
     # ki se klice ob prihodu na to pot
-    webapp2.Route('/sporocilo/<sporocilo_id:\d+>', PosameznoSporociloHandler)
+    webapp2.Route('/sporocilo/<sporocilo_id:\d+>', PosameznoSporociloHandler),
+    webapp2.Route('/sporocilo/<sporocilo_id:\d+>/edit', UrediHandler),
+    webapp2.Route('/sporocilo/<sporocilo_id:\d+>/delete', IzbrisiHandler)
 ], debug=True)
